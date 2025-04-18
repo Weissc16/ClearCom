b7da9ff77ad0c4c40f2f01656ba96264467c7ec5
Cody Weiss <weissc16@gmail.com>
2025-04-18 11:29:51 -0600

    Added database model, created class User, updated run.py to create database tables.

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from app import create_app
+from app import create_app, db
+import sys
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "create_db":
+        with app.app_context():
+            db.create_all()
+            print("Database tables created.")
+    else:
+        app.run(debug=True)
